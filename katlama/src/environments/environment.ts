c82d2ec0ab84c7a48fc1a28864a606f0171e9cf5
murat onur <mokosam@gmail.com>
2021-05-02 00:34:55 +0300

    arweavedo

--- katlama/src/environments/environment.ts
+++ katlama/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   appName: 'katlama',
   appVersion: '0.0.0',
-  arweaveDomain:
+  arweaveDomain1:
     'https://arweave.net/kPueF_RwxVop6pBOED5c3G1C5Osnvd4uP7K_BJ84AmU',
 };
